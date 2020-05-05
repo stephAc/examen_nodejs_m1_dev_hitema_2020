3533f7583dd26bc12711cafb5f513bb989c0986b
Stéphane <stephane.achari@primeradiant.fr>
2020-05-05 16:42:02 +0200

    websocket

--- websocket/websocket-server.js
+++ websocket/websocket-server.js
@@ -1,15 +1,18 @@
-const WebSocket = require('ws')
+const WebSocket = require('ws');
 const webSocketServer = new WebSocket.Server({ port: 3003 });
-  
-webSocketServer.on('connection', webSocket => {
-    webSocket.onmessage = messageEvent => {
-        const message = messageEvent.data;
-        webSocketServer.clients.forEach(function each(client) {
-            if (client.readyState === WebSocket.OPEN) {
-                client.send(message);
-            }
-        });
-    };
+let discussion = [];
+
+webSocketServer.on('connection', (webSocket) => {
+  webSocket.send(JSON.stringify(discussion));
+  webSocket.onmessage = (messageEvent) => {
+    const message = messageEvent.data;
+    discussion.push(message);
+    webSocketServer.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  };
 });
 
 module.exports = webSocketServer;
