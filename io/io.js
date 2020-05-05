9b6542679aec39da889ca3b1fb61f2909600095a
Stéphane <stephane.achari@primeradiant.fr>
2020-05-05 17:09:26 +0200

    io

--- io/io.js
+++ io/io.js
@@ -2,9 +2,10 @@ const fs = require('fs');
 
 module.exports.decodeHexFileContent = (filePath) => {
   return new Promise((resolve, reject) => {
-    fs.readFile(filePath, 'hex');
-    const convert = (from, to) => (str) => Buffer.from(str, from).toString(to);
-    const utf8ToHex = convert('utf8', 'hex');
-    const hexToUtf8 = convert('hex', 'utf8');
+    fs.readFile(filePath, (err, contents) => {
+      if (err) reject();
+      const res = Buffer.from(contents.toString(), 'hex').toString();
+      resolve(res);
+    });
   });
 };
